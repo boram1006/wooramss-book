055e0ebb9aca4243fc470d5e442b053c2d5dbc3f
boram1006 <casey1006@gmail.com>
2026-08-17 16:11:51 +0900

    refine: keep dialogic questions open ended

--- test/book-guide.test.js
+++ test/book-guide.test.js
@@ -97,6 +97,7 @@ test('출력 값의 중복 테마와 불필요한 공백을 정리한다', () =>
 test('프롬프트는 짧은 열린 질문과 안전한 놀이 형식을 강제한다', () => {
   assert.match(SYSTEM_PROMPT, /질문은 정확히 2개/);
   assert.match(SYSTEM_PROMPT, /부모 마음 맞히기/);
+  assert.match(SYSTEM_PROMPT, /두 가지 선택지를 제시하지 않습니다/);
   assert.match(SYSTEM_PROMPT, /준비물: A, B\. 방법: ①/);
   assert.match(SYSTEM_PROMPT, /48개월 미만/);
 });
